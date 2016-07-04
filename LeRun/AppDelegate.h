01dd4776e64b409ec386cb412ad90b15e4a5d207
pptk <pengxionghuidemacpro@pengxiomacdembp.lan>
2016-07-04 15:36:01 +0800

    Initial Commit

---
+++ LeRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeRun
+//
+//  Created by 彭雄辉的Mac Pro on 16/7/4.
+//  Copyright © 2016年 彭雄辉的Mac Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeRun/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LeRun
+//
+//  Created by 彭雄辉的Mac Pro on 16/7/4.
+//  Copyright © 2016年 彭雄辉的Mac Pro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
